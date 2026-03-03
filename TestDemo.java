8f8db8652cf60a041d72c41879ba6bd04a49145f
gouravprajapati2003-cmd <gouravprajapati8132@gmail.com>
2026-03-03 22:16:15 +0530

    Default Access

---
+++ TestDemo.java
@@ -0,0 +1,11 @@
+class Test {
+    String name;
+}
+public class TestDemo {
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.name = "Gourav Kumar";
+        System.out.println("Your Name is : "+ t.name);
+
+    }
+}
