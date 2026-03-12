a1352a118066ab21811d6defac609763db542193
gouravprajapati2003-cmd <gouravprajapati8132@gmail.com>
2026-03-12 23:07:10 +0530

    Updated

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,13 +1,20 @@
 public class ArrayDemo {
     public static void main(String[] args) {
         int[] arr = new int[5];
+
         arr[0]=10;
         arr[1]=20;
         arr[2]=30;
         arr[3]=40;
         arr[4]=50;
+
+        System.out.print("[");
         for(int i=0; i<arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.print(arr[i]);
+            if(i < arr.length-1) {
+                System.out.print(", ");
+            }
         }
+        System.out.print("]");
     }
 }
